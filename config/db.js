a0eeb8e8df88d7363efbf4ed0751818fd53a0081
Shivamkadam692 <shivam.123kadam@gmail.com>
2025-08-20 23:32:16 +0530

    added

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/transh');
+    await mongoose.connect('mongodb+srv://ktiku5392:XkWzXfeBWDPJeL8E@t2yd.sfyhphm.mongodb.net/?retryWrites=true&w=majority&appName=T2YD');
     console.log('MongoDB Connected');
   } catch (err) {
     console.error('MongoDB connection failed:', err.message);
